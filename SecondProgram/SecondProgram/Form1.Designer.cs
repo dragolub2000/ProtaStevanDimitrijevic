21f7f41b7bb6c8092fd3e7647f104104f4c8d065
Dragan Golubovic <dragolub2000@yahoo.com>
2022-09-13 16:02:55 +0200

    Verzija 1.0.0

---
+++ SecondProgram/SecondProgram/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace SecondProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listStudents = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtIme = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrezime = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Prikaži";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listStudents
+            // 
+            this.listStudents.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listStudents.HideSelection = false;
+            this.listStudents.Location = new System.Drawing.Point(12, 12);
+            this.listStudents.Name = "listStudents";
+            this.listStudents.Size = new System.Drawing.Size(496, 201);
+            this.listStudents.TabIndex = 2;
+            this.listStudents.UseCompatibleStateImageBehavior = false;
+            this.listStudents.View = System.Windows.Forms.View.Details;
+            this.listStudents.Click += new System.EventHandler(this.listStudents_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ID";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Name";
+            this.columnHeader2.Width = 70;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Predmet";
+            this.columnHeader3.Width = 110;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 245);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ime";
+            // 
+            // txtIme
+            // 
+            this.txtIme.Location = new System.Drawing.Point(82, 245);
+            this.txtIme.Name = "txtIme";
+            this.txtIme.Size = new System.Drawing.Size(344, 22);
+            this.txtIme.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 302);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Prezime";
+            // 
+            // txtPrezime
+            // 
+            this.txtPrezime.Location = new System.Drawing.Point(82, 302);
+            this.txtPrezime.Name = "txtPrezime";
+            this.txtPrezime.Size = new System.Drawing.Size(344, 22);
+            this.txtPrezime.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(528, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Dodaj radnika";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(528, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(149, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Измени";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(528, 145);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(149, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Избриши";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 508);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtPrezime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtIme);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listStudents);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listStudents;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtIme;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrezime;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ SecondProgram/SecondProgram/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Windows.Markup;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Reflection;
+
+namespace SecondProgram
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //DESKTOP-JN7AEUR
+            this.LoadView();
+        }
+        private void LoadView()
+        {
+            string ComputerName = Environment.MachineName;
+            string ConString = @"Data Source=" + ComputerName + ";Initial Catalog=testdatabase;Integrated Security=True";
+            SqlConnection con = new SqlConnection(ConString);
+            string querystring = "SELECT * FROM Radnik2";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(querystring, con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            int i = 0;
+            this.listStudents.Items.Clear();
+            while (reader.Read())
+            {
+                this.listStudents.Items.Add(reader["id"].ToString());
+
+                this.listStudents.Items[i].SubItems.Add(reader["Ime"].ToString());
+                this.listStudents.Items[i].SubItems.Add(reader["Prezime"].ToString());
+                i++;
+            }
+            con.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ComputerName = Environment.MachineName;
+                string ConString = @"Data Source=" + ComputerName + ";Initial Catalog=testdatabase;Integrated Security=True";
+                SqlConnection con = new SqlConnection(ConString);
+                string Ime = txtIme.Text;
+                String Prezime = txtPrezime.Text;
+                string querystring = "INSERT INTO Radnik2 (Ime,Prezime) "
+                    + "VALUES (@Ime,@Prezime)";
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand(querystring, con);
+                cmd.Parameters.AddWithValue("@Ime",Ime);
+                cmd.Parameters.AddWithValue("@Prezime", Prezime);
+                // izvrsi sql upit
+                cmd.ExecuteNonQuery();
+                con.Close();
+                // prikazi list view opet
+                this.LoadView();
+                MessageBox.Show("Radnik uspesno unet u bazu podataka!");
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Postoji problem: " + ex.Message); ;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.LoadView();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.listStudents.SelectedItems.Count == 0)
+                    return;
+
+                string id = this.listStudents.SelectedItems[0].Text;
+                //MessageBox.Show(id);
+
+                string ComputerName = Environment.MachineName;
+                string ConString = @"Data Source=" + ComputerName + ";Initial Catalog=testdatabase;Integrated Security=True";
+                SqlConnection con = new SqlConnection(ConString);
+                string Ime = txtIme.Text;
+                String Prezime = txtPrezime.Text;
+                string querystring = "UPDATE Radnik2 SET Ime=@Ime,Prezime=@Prezime WHERE  ID=@id";
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand(querystring, con);
+                cmd.Parameters.AddWithValue("@Ime", Ime);
+                cmd.Parameters.AddWithValue("@Prezime", Prezime);
+                cmd.Parameters.AddWithValue("@id", id);
+                // izvrsi sql upit
+                cmd.ExecuteNonQuery();
+                con.Close();
+                // prikazi list view opet
+                this.LoadView();
+                MessageBox.Show("Podaci o radniku uspesno promenjeni u bazi podataka u bazu podataka!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Postoji problem: " + ex.Message); ;
+            }
+        }
+
+        private void listStudents_Click(object sender, EventArgs e)
+        {
+            var item = this.listStudents.SelectedItems[0];
+            string Ime = item.SubItems[1].Text;
+            string Prezime = item.SubItems[2].Text;
+            this.txtIme.Text = Ime;
+            this.txtPrezime.Text = Prezime;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.listStudents.SelectedItems.Count == 0)
+                    return;
+
+                string id = this.listStudents.SelectedItems[0].Text;
+
+                string ComputerName = Environment.MachineName;
+                string ConString = @"Data Source=" + ComputerName + ";Initial Catalog=testdatabase;Integrated Security=True";
+                SqlConnection con = new SqlConnection(ConString);
+                string querystring = "DELETE FROM Radnik2  WHERE  ID=@id";
+
+                con.Open();
+                SqlCommand cmd = new SqlCommand(querystring, con);
+                cmd.Parameters.AddWithValue("@id", id);
+                // izvrsi sql upit
+                cmd.ExecuteNonQuery();
+                con.Close();
+                // prikazi list view opet
+                this.LoadView();
+                MessageBox.Show("Podaci o radniku uspesno izbrisani u bazi podataka u bazu podataka!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Postoji problem: " + ex.Message); ;
+            }
+        }
+    }
+}
